d10f9c1cfb0431794b9bacec9301606ad05fd110
Emiliano Gandini Outeda <104856039+emiliano-gandini-outeda@users.noreply.github.com>
2025-06-14 00:56:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Eclipse Growth Optimization Services
+# Copyright (C) 2025 Emiliano Gandini Outeda
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
